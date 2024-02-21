05da43b36ac443054a38d9548fee14cd7aa2bb1e
Shivam Kumar <goodwaygiver143@gmail.com>
2024-02-21 22:20:52 +0530

    trying to deploy 3

--- index.js
+++ index.js
@@ -191,8 +191,8 @@ app.get('/download-alraudio', async (req, res) => {
 
 
 
-// // Start the server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// });
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
