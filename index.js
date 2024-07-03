8b3b6fc107b86d3880ea1cb3bc10a49ee8dbbbba
Shivam Kumar <goodwaygiver143@gmail.com>
2024-07-03 09:03:16 +0530

    hello nothing

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ import ffmpeg from "fluent-ffmpeg";
 import path from "path";
 const __dirname = path.resolve();
 import cors from "cors";
+import fs from "fs";
+import streamBuffers from "stream-buffers";
+import ufs from "url-file-size";
+import sanitizeFilename from "sanitize-filename";
+import contentDisposition from "content-disposition";
 
 // import ffmpegPath from "ffmpeg-static";
 import cp from "child_process";
@@ -144,6 +149,123 @@ const ytmixer = (link, itag, options = {}) => {
     });
   return result;
 };
+// ----------
+
+// const ytmixer = (link, itag, options = {}) => {
+//   const result = new stream.PassThrough({
+//     highWaterMark: options.highWaterMark || 1024 * 512,
+//   });
+
+//   let totalSize = 0;
+
+//   ytdl
+//     .getInfo(link, options)
+//     .then((info) => {
+//       const videoFormat = info.formats.find((format) => format.itag === itag);
+
+//       if (!videoFormat) {
+//         throw new Error("No such format found");
+//       }
+
+//       let audioStream = ytdl.downloadFromInfo(info, {
+//         ...options,
+//         quality: "highestaudio",
+//       });
+//       let videoStream = ytdl.downloadFromInfo(info, {
+//         ...options,
+//         format: videoFormat,
+//       });
+
+//       let ffmpegProcess = cp.spawn(
+//         ffmpegPath,
+//         [
+//           "-loglevel",
+//           "8",
+//           "-hide_banner",
+//           "-i",
+//           "pipe:3",
+//           "-i",
+//           "pipe:4",
+//           "-map",
+//           "0:a",
+//           "-map",
+//           "1:v",
+//           "-c",
+//           "copy",
+//           "-f",
+//           "matroska",
+//           "pipe:5",
+//         ],
+//         {
+//           windowsHide: true,
+//           stdio: ["inherit", "inherit", "inherit", "pipe", "pipe", "pipe"],
+//         }
+//       );
+
+//       audioStream.pipe(ffmpegProcess.stdio[3]);
+//       videoStream.pipe(ffmpegProcess.stdio[4]);
+
+//       ffmpegProcess.stdio[5].on("data", (chunk) => {
+//         totalSize += chunk.length;
+//       });
+
+//       ffmpegProcess.stdio[5].pipe(result);
+
+//       ffmpegProcess.on("close", () => {
+//         result.emit("size", totalSize);
+//       });
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//       result.emit("error", error);
+//     });
+
+//   return {
+//     stream: result,
+//     getSize: () => totalSize,
+//   };
+// };
+
+// app.get("/download", async (req, res) => {
+//   try {
+//     const videoURL = req.query.url;
+//     const itag = parseInt(req.query.itag); // Convert itag to integer
+
+//     if (!videoURL || !itag) {
+//       return res.status(400).send("Video URL and itag are required");
+//     }
+
+//     const info = await ytdl.getInfo(videoURL);
+
+//     const format = info.formats.find(
+//       (format) => format.itag === parseInt(itag)
+//     );
+
+//     const videoStream = ytmixer(videoURL, itag);
+
+//     // Set Content-Disposition header to force download
+//     const sanitizedTitle = sanitizeFilename(info.videoDetails.title) || "video";
+
+//     const contentLength = parseInt(format.contentLength) || 0;
+
+//     // Use content-disposition to set the Content-Disposition header
+//     const disposition = contentDisposition(
+//       `${sanitizedTitle}.${format.container}`
+//     );
+//     res.setHeader("Content-Disposition", disposition);
+
+//     // }
+//     res.setHeader("Content-Length", videoStream.getSize());
+//     res.setHeader("Content-Type", `video/${format.container}`);
+//     console.log("videoStream.totalVdoSize ", videoStream.totalVdoSize);
+//     // Pipe the video stream to the response
+//     videoStream.stream.pipe(res);
+//     // res.json({sucess: true})
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
 
 app.get("/download", async (req, res) => {
   try {
@@ -155,74 +277,126 @@ app.get("/download", async (req, res) => {
     }
 
     const info = await ytdl.getInfo(videoURL);
-    const sanitizedTitle = info.videoDetails.title
-      .replace(/[^a-z0-9]+/gi, "_")
-      .replace(/^_+|_+$/g, ""); // Replace invalid characters with underscores
-    const myFormat = info.formats.filter((format) => {
-      return format.itag === itag;
-    });
 
-    // console.log(myFormat)
-    let contentLengthInBytes = myFormat[0].contentLength;
-    // Convert bytes to megabytes
-    const contentLengthInMB = Math.ceil(contentLengthInBytes / (1000 * 1000));
+    const format = info.formats.find((format) => format.itag === itag);
 
-    console.log("File size:", contentLengthInMB, "MB");
-    const videoStream = ytmixer(videoURL, itag);
+    if (!format) {
+      return res.status(400).send("Invalid itag or format not found");
+    }
 
-    // Set Content-Disposition header to force download
-    res.header(
-      "Content-Disposition",
-      `attachment; filename="${sanitizedTitle}.mp4"`
-    );
-    res.header("Content-Type", "video/mp4");
-    res.header("Filename", sanitizedTitle);
+    if (format.hasAudio) {
+      const sanitizedTitle =
+        sanitizeFilename(info.videoDetails.title) || "video";
 
-    // Pipe the video stream to the response
-    videoStream.pipe(res);
-    // res.json({sucess: true})
+      const contentLength = parseInt(format.contentLength) || 0;
+
+      // Use content-disposition to set the Content-Disposition header
+      const disposition = contentDisposition(
+        `${sanitizedTitle}.${format.container}`
+      );
+      res.setHeader("Content-Disposition", disposition);
+      // res.setHeader('Content-Disposition', `attachment; filename="${info.videoDetails.title}.${format.container}"`);
+
+      if (contentLength !== 0) {
+        res.setHeader("Content-Length", contentLength);
+      } else {
+        let mylength = await ufs(format.url);
+        res.setHeader("Content-Length", mylength);
+      }
+      res.setHeader("Content-Type", `video/${format.container}`);
+      let myone = ytdl(videoURL, { format });
+
+      ytdl(videoURL, { format }).pipe(res);
+    } else {
+      const videoStream = ytmixer(videoURL, itag);
+
+      // Buffer the video data
+      // const writableStreamBuffer = new streamBuffers.WritableStreamBuffer();
+      // videoStream.stream.pipe(writableStreamBuffer);
+
+      // videoStream.stream.on("end", () => {
+      //   const buffer = writableStreamBuffer.getContents();
+      //   if (buffer) {
+      //     const contentLength = buffer.length;
+      //     const sanitizedTitle =
+      //       sanitizeFilename(info.videoDetails.title) || "video";
+      //     const disposition = contentDisposition(
+      //       `${sanitizedTitle}.${format.container}`
+      //     );
+      //     console.log("contentLength hai yha  ", contentLength);
+      //     res.setHeader("X-Video-Duration", format.approxDurationMs);
+      //     res.setHeader("Content-Disposition", disposition);
+      //     res.setHeader("Content-Length", contentLength);
+      //     res.setHeader("Content-Type", `video/${format.container}`);
+      //     res.send(buffer);
+      //   } else {
+      //     res.status(500).send("Failed to buffer video data");
+      //   }
+      // });
+
+      videoStream.on("error", (error) => {
+        console.error("Stream Error:", error);
+        res.status(500).send("<h1>Internal Server Error</h1>");
+      });
+    }
   } catch (error) {
     console.error(error);
-    res.status(500).send("Internal Server Error");
+    res.status(500).send("<h1>Internal Server Error</h1>");
   }
 });
 
 app.get("/download-allreadyaudio", async (req, res) => {
-  try {
-    const videoURL = req.query.url;
-    const itag = parseInt(req.query.itag); // Convert itag to integer
+  const videoURL = req.query.url;
+  const itag = req.query.itag;
+  if (!videoURL || !itag) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Video URL and itag are required" });
+  }
 
-    if (!videoURL || !itag) {
-      return res.status(400).send("Video URL and itag are required");
+  try {
+    const info = await ytdl.getInfo(videoURL);
+    const format = info.formats.find(
+      (format) => format.itag === parseInt(itag)
+    );
+
+    if (!format) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Video format not found" });
     }
 
-    const info = await ytdl.getInfo(videoURL);
+    // Sanitize the video title
+    const sanitizedTitle = sanitizeFilename(info.videoDetails.title) || "video";
 
-    const sanitizedTitle = info.videoDetails.title.replace(/[^a-z0-9]+/gi, "_");
+    const contentLength = parseInt(format.contentLength) || 0;
 
-    // Get video stream directly filtering by itag
-    const videoStream = ytdl(videoURL, {
-      filter: (format) => format.itag === itag,
-    });
-
-    // Set response headers
-    res.header(
-      "Content-Disposition",
-      `attachment; filename="${sanitizedTitle}.mp4"`
+    // Use content-disposition to set the Content-Disposition header
+    const disposition = contentDisposition(
+      `${sanitizedTitle}.${format.container}`
     );
-    res.header("Content-Type", "video/mp4");
+    res.setHeader("Content-Disposition", disposition);
+    // res.setHeader('Content-Disposition', `attachment; filename="${info.videoDetails.title}.${format.container}"`);
 
-    res.header("Filename", sanitizedTitle);
+    if (contentLength !== 0) {
+      res.setHeader("Content-Length", contentLength);
+    } else {
+      let mylength = await ufs(format.url);
+      res.setHeader("Content-Length", mylength);
+    }
+    res.setHeader("Content-Type", `video/${format.container}`);
+    let myone = ytdl(videoURL, { format });
 
-    // Pipe video stream to response
-    videoStream.pipe(res);
+    ytdl(videoURL, { format }).pipe(res);
   } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
+    console.error("Error downloading video:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Error downloading video" });
   }
 });
 
-console.log("FFmpeg Path:", ffmpegPath);
+// console.log("FFmpeg Path:", ffmpegPath);
 
 // Start the server
 const PORT = process.env.PORT || 5000;
